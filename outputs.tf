a018523942cb776f36b4552ad9ef253d5988d466
Jean 'clark' EYMERT <jean@akiros.io>
2024-06-10 12:00:29 +0200

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,19 @@ output "iam_role_step_function" {
   value = aws_iam_role.step_function.arn
 }
 
+output "state_machine_arn" {
+  value = aws_sfn_state_machine.refresh_env.arn
+}
+
 output "state_machine_name" {
   value = aws_sfn_state_machine.refresh_env.name
 }
 
-output "step_function_json_files" {
+output "step_function_json_files_local_path" {
+  value = local_file.step_function_json_input.filename
+}
+
+output "step_function_json_files_s3_path" {
   value = {
     for app_name, object in aws_s3_object.step_function_json_input :
     app_name => "s3://${object.bucket}/${object.key}"
